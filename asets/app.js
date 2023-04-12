73d3ab7386bbbcc98b6269a264f82de2d2096b4e
Gooddden <goodden86@gmail.com>
2023-04-12 09:31:58 +0300

    error 10 строка

---
+++ asets/app.js
@@ -0,0 +1,26 @@
+
+let number = +prompt ('Введите любое целое число');
+console.log(`введенное число`);
+
+    let Prime_number = true ;
+    console.log(`flag1`);
+
+for (let i=2 ; i < number ; i++ ){
+
+    if (nomber % i == 0) {
+        
+        Prime_number = false;
+        break;
+
+    } else {
+        
+    let check = number / i;
+    }
+
+    }
+    if(Prime_number){
+        alert(`Число ${number}- простое`);
+
+    } else {
+            alert(`Число ${number}-составное`);
+    }
